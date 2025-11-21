8441f0f93ac9bbc3d3e2aec8f7a1b5ccaf984842
madhura1112 <madhurabiradar510@gmail.com>
2025-11-21 12:51:20 +0530

    Add files via upload

---
+++ rajkumar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void names (char first[], char last[],int sw)
+{
+    int i=0;
+    char full[100];
+    while (first[i]!='\0') // adding first
+    {
+    full[i] =first[i];
+    i++;
+}
+
+full[i]=' '; // adding space
+i++;
+int j=0;
+while (last[j]!='\0') // adding last
+{
+full[i]=last[j];
+i++;
+j++;
+}
+full[i]=' ';
+i++;
+printf("%s", full);
+int len=i+1;
+if(len<sw)
+printf("fit in the screen");
+else
+printf("does not fit in the screen!");
+}
+
+void main()
+{
+    char first[100]="mad";
+    char last [100]="mala";
+    int sw=20;
+    names(first,last,sw);
+
+}
